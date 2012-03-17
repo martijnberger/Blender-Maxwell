40e58d8d6de132da48def5abe985985464393ad8
Martijn Berger <martijn.berger@gmail.com>
2012-03-17 15:47:20 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,70 @@
+bl_info = {
+    "name": "NextLimit Maxwell format",
+    "author": "Martijn Berger",
+    "blender": (2, 6, 0),
+    "location": "File > Import-Export",
+    "description": "Import-Export MXS",
+    "warning": "Very early preview",
+    "wiki_url": "http://www.nextlimit.com",
+    "tracker_url": "",
+    "category": "Import-Export"}
+
+if "bpy" in locals():
+    import imp
+    if "import_mxs" in locals():
+        imp.reload(import_mxs)
+
+
+import bpy
+from bpy.props import (BoolProperty,
+                       FloatProperty,
+                       StringProperty,
+                       EnumProperty,
+                       )
+from bpy_extras.io_utils import (ExportHelper,
+                                 ImportHelper,
+                                 path_reference_mode,
+                                 axis_conversion,
+                                 )
+
+
+class ImportMXS(bpy.types.Operator, ImportHelper):
+    '''load a NextLimit Maxwell MXS file'''
+    bl_idname = "import_scene.mxs"
+    bl_label = "Import MXS"
+    bl_options = {'PRESET', 'UNDO'}
+
+    filename_ext = ".mxs"
+
+    filter_glob = StringProperty(
+            defaults="*.mxs",
+            options={'HIDDEN'},
+            )
+
+    def execute(self, context):
+        from . import import_mxs
+        keywords = self.as_keywords(ignore=("axis_forward",
+                                "axis_up",
+                                "filter_glob",
+                                "split_mode",
+                                ))
+
+
+        return import_mxs.load(self, context, **keywords)
+
+    def draw(self, context):
+        layout = self.layout
+
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportMXS.bl_idname, text="Maxwell (.mxs)")
+
+def register():
+    bpy.utils.register_module(__name__)
+
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
---
+++ import_mxs.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import time
+import bpy
+import mathutils
+
+sys.path.append('/opt/maxwell/python/pymaxwell/python3.2/')
+
+from bpy_extras.io_utils import unpack_list, unpack_face_list
+from bpy_extras.image_utils import load_image
+
+from pymaxwell import *;
+
+def load(operator, context, filepath):
+
+    print('\nimporting mxs %r' % filepath)
+
+    filepath = os.fsencode(filepath)
+
+    time_main = time.time()
+    
+    mxs_scene = Cmaxwell(mwcallback)
+    
+    ok = mxs_scene.readMXS(str(filepath))
+    if ok == 0:
+        print('\nError reading input file: %r' % (filepath))
+    
+    time_new = time.time()
+    print('\nDone parsing mxs %r in %.4f sec.' % (filepath, (time_new - time_main)))
+
+    time_new = time.time()
+    print('finished importing: %r in %.4f sec.' % (filepath, (time_new - time_main)))
+    return {'FINISHED'}
+
+
+
